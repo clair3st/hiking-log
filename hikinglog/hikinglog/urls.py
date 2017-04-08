ce7997dac48c7c12f9662fe84daec193dd575a8d
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-04-08 12:00:26 -0700

    added home url

--- hikinglog/hikinglog/urls.py
+++ hikinglog/hikinglog/urls.py
@@ -1,4 +1,4 @@
-"""hikinglog URL Configuration
+"""hikinglog URL Configuration.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', TemplateView.as_view(template_name="hikinglog/home.html"),
+        name='home'),
 ]
