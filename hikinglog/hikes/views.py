7d34eaf44faabc9396b82592adfdfbbb537ba444
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-04-10 17:33:19 -0700

    added views for edit and create a hike

--- hikinglog/hikes/views.py
+++ hikinglog/hikes/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+"""View Class for hiking app."""
 
-# Create your views here.
+from hikes.models import Hikes
+from django.views.generic import (CreateView,
+                                  UpdateView)
+from django.urls import reverse_lazy
+
+
+class CreateHike(CreateView):
+    """Add a hike."""
+
+    template_name = 'hikes/new_hike.html'
+    model = Hikes
+    fields = ['name',
+              'date',
+              'gain',
+              'height',
+              'duration',
+              'distance',
+              'region',
+              'park',
+              'weather',
+              'notes',
+              'lat',
+              'lng']
+    success_url = reverse_lazy("home")
+
+
+class EditHike(UpdateView):
+    """Edit a hike."""
+
+    template_name = "hikes/edit_hike.html"
+    success_url = reverse_lazy("home")
+    model = Hikes
+    fields = ['name',
+              'date',
+              'gain',
+              'height',
+              'duration',
+              'distance',
+              'region',
+              'park',
+              'weather',
+              'notes',
+              'lat',
+              'lng']
