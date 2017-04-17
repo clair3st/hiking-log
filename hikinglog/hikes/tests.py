0586932e89a15089aec0a33e7cd8a48ac34643ef
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-04-16 17:55:16 -0700

    tests for hikes model setup

--- hikinglog/hikes/tests.py
+++ hikinglog/hikes/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+"""Test hike models and views."""
 
-# Create your tests here.
+from django.test import TestCase
+import factory
+from hikes.models import Hike
+
+
+class HikeFactory(factory.django.DjangoModelFactory):
+    """Create hikes to test."""
+
+    class Meta:
+        """Invoke Hike instance."""
+
+        model = Hike
+
+    name = factory.Faker('title')
+
+
+class HikeTestCase(TestCase):
+    """Test hikes."""
+
+    def setUp(self):
+        """The setup and buildout for hikes."""
+        self.hikes = [HikeFactory.create() for i in range(10)]
